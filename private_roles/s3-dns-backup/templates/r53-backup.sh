839eb68fedd0753f978b1f57f08127f75d29ace8
Jon Cortmaior <jon.cortmaior@istresearch.com>
2017-04-07 08:58:28 -0600

    added path to aws calls

--- private_roles/s3-dns-backup/templates/r53-backup.sh
+++ private_roles/s3-dns-backup/templates/r53-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 today=`date  +"%m-%d-%Y"`
 mkdir $today
-aws route53 list-hosted-zones | jq -r  '.HostedZones[] | .Id +" "+ .Name + " " + [.Config.PrivateZone | tostring][0]' |
+/usr/local/bin/aws route53 list-hosted-zones | /usr/bin/jq -r  '.HostedZones[] | .Id +" "+ .Name + " " + [.Config.PrivateZone | tostring][0]' |
 while read zoneentry 
 do
 read -a zoneinfo <<< "$zoneentry"
@@ -17,10 +17,10 @@ esac
 zonefilename="$justzoneid-$zonename$zonestatus"
 jsonfilename="$zonefilename.json"
 textfilename="$zonefilename.txt"
-aws route53 list-resource-record-sets --hosted-zone-id  $justzoneid > $today/$jsonfilename
-aws route53 list-resource-record-sets --output text --hosted-zone-id  $justzoneid > $today/$textfilename
-aws s3 cp $today/$jsonfilename s3://dns-backup-ist/$today/
-aws s3 cp $today/$textfilename s3://dns-backup-ist/$today/
+/usr/local/bin/aws route53 list-resource-record-sets --hosted-zone-id  $justzoneid > $today/$jsonfilename
+/usr/local/bin/aws route53 list-resource-record-sets --output text --hosted-zone-id  $justzoneid > $today/$textfilename
+/usr/local/bin/aws s3 cp $today/$jsonfilename s3://dns-backup-ist/$today/
+/usr/local/bin/aws s3 cp $today/$textfilename s3://dns-backup-ist/$today/
 done
 rm -rf $today
 
